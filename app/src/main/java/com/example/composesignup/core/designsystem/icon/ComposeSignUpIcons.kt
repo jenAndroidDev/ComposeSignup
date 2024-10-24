dceb821199a3c0cdc51fe6125250e6369838a6e3
jenAndroidDev <jendev971@gmail.com>
2024-10-24 12:32:29 +0530

    wip/signup validation.

--- app/src/main/java/com/example/composesignup/core/designsystem/icon/ComposeSignUpIcons.kt
+++ app/src/main/java/com/example/composesignup/core/designsystem/icon/ComposeSignUpIcons.kt
@@ -2,13 +2,13 @@ package com.example.composesignup.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Icon
+import androidx.compose.ui.res.painterResource
+import com.example.composesignup.R
 
 object ComposeSignUpIcons {
     val HomeSelected = Icons.Default.Home
@@ -18,5 +18,7 @@ object ComposeSignUpIcons {
     val Profile = Icons.Outlined.Person
     val Email = Icons.Filled.MailOutline
     val PasswordLock = Icons.Outlined.Lock
-
+}
+object ComposeSignupVectors{
+    val SuccessTick = R.drawable.ic_success_tick
 }
--- app/src/main/java/com/example/composesignup/feature/onboard/presentation/SignUpScreen.kt
+++ app/src/main/java/com/example/composesignup/feature/onboard/presentation/SignUpScreen.kt
@@ -3,22 +3,32 @@ package com.example.composesignup.feature.onboard.presentation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.composesignup.core.designsystem.components.ComposeSignUpTextField
 import com.example.composesignup.core.designsystem.icon.ComposeSignUpIcons
+import com.example.composesignup.core.designsystem.icon.ComposeSignupVectors
 import com.example.composesignup.ui.theme.GREY20
+import com.example.composesignup.ui.theme.GREY80
 import kotlinx.coroutines.flow.StateFlow
 
 /*
@@ -96,16 +106,50 @@ fun SignUpTextFields(
     }
 }
 @Composable
-fun SignUpValidation(
+fun SignUpValidationList(
+modifier: Modifier,
 viewModel: SignUpViewModel,
 action: (SignUpUiAction) -> Unit
 ){
     Column {
-        LazyColumn {
+        val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+        val validationMsg = uiState.value.validationMessages
+        LazyColumn(modifier = modifier) {
+            items(count = validationMsg.size,
+                ){
 
+            }
         }
     }
 
+}
+
+@Composable
+fun ValidationItem(
+    modifier: Modifier = Modifier,
+    message:String = "Atleast 8 Characters",
+    isValid:Boolean = false
+){
+    Row(modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        ) {
+        Icon(
+            painterResource(ComposeSignupVectors.SuccessTick),
+            modifier = modifier.size(18.dp),
+            contentDescription = null)
+        Text(text = message,
+            modifier = modifier.padding(start = 8.dp),
+            style = MaterialTheme.typography.labelSmall,
+            color = GREY80
+            )
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewValidationItem(){
+    ValidationItem()
 }
 
 @Preview
