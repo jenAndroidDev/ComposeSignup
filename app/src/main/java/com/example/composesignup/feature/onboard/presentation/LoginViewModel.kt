8cf4d58eb2f1ecd1768713eb5a6a6c3467417e18
jenAndroidDev <jendev971@gmail.com>
2024-11-23 11:35:08 +0530

    Added Text Fields and Login Button in LoginScreen.kt

--- app/src/main/java/com/example/composesignup/feature/onboard/presentation/LoginViewModel.kt
+++ app/src/main/java/com/example/composesignup/feature/onboard/presentation/LoginViewModel.kt
@@ -1,13 +1,26 @@
 package com.example.composesignup.feature.onboard.presentation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.composesignup.utlis.UiText
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 private const val Tag = "LoginViewModel"
 class LoginViewModel:ViewModel() {
+
+    /**
+     * */
+    var email by mutableStateOf("")
+        private set
+
+    var password by mutableStateOf("")
+        private set
 
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState = _uiState.asStateFlow()
@@ -17,11 +30,24 @@ class LoginViewModel:ViewModel() {
     init {
 
         action ={
-            onUiAction()
+            onUiAction(it)
         }
     }
-    private fun onUiAction(){
+    private fun onUiAction(action: LoginUiAction){
+        when(action){
+            is LoginUiAction.Login->{
 
+            }
+            is LoginUiAction.Password->{
+                password = action.password
+            }
+            is LoginUiAction.Email->{
+                email = action.email
+            }
+            is LoginUiAction.ForgotPassword->{
+
+            }
+        }
     }
 
 }
@@ -32,4 +58,7 @@ data class LoginUiState(
 )
 sealed class LoginUiAction{
     data object Login:LoginUiAction()
+    data object ForgotPassword:LoginUiAction()
+    data class Email(val email:String):LoginUiAction()
+    data class Password(val password:String):LoginUiAction()
 }
