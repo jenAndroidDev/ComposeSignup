c981dbbf0c339ee175577f3f6e24c2188414cc1d
jenAndroidDev <jendev971@gmail.com>
2024-11-30 14:32:12 +0530

    Add App Logo in OnboardScreen.kt ✅

--- app/src/main/java/com/example/composesignup/feature/onboard/presentation/OnboardScreen.kt
+++ app/src/main/java/com/example/composesignup/feature/onboard/presentation/OnboardScreen.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.outlined.Home
@@ -29,26 +31,30 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composesignup.R
+import com.example.composesignup.core.designsystem.components.AppHeader
 import com.example.composesignup.ui.theme.GREY20
 import com.example.composesignup.ui.theme.Green80
+import com.example.composesignup.ui.theme.fontFamily
 import kotlinx.coroutines.launch
 
+private const val Tag = "OnboardScreen"
 @Composable
 fun OnboardScreen(
     modifier: Modifier,
 ){
-    Column(modifier = modifier.fillMaxSize()
+    Column(modifier = modifier
+        .fillMaxSize()
         .background(color = GREY20)
         .windowInsetsPadding(WindowInsets.statusBars)) {
         Header(modifier = modifier)
         OnboardTabContainer(modifier = Modifier)
     }
-
 }
-
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun OnboardTabContainer(modifier: Modifier){
@@ -56,15 +62,20 @@ private fun OnboardTabContainer(modifier: Modifier){
         OnboardTabItem(
             selectedIcon = Icons.Filled.Home,
             unselectedIcon = Icons.Outlined.Home,
-            "Login"
+            description = stringResource(R.string.login)
         ),
         OnboardTabItem(
             selectedIcon = Icons.Filled.Home,
             unselectedIcon = Icons.Outlined.Home,
-            "Sign up"
+            description = stringResource(R.string.sign_up)
         ))
     val pagerState = rememberPagerState(pageCount = { tabItems.size })
-    Column(modifier = modifier.fillMaxWidth()) {
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .background(
+            shape = RoundedCornerShape(RoundedCornerRadius),
+            color = Color.White
+        )) {
         val coroutineScope = rememberCoroutineScope()
         TabRow(
             containerColor = Color.White,
@@ -108,27 +119,41 @@ private fun OnboardTabContainer(modifier: Modifier){
         }
     }
 }
+
 @Composable
-private fun Header(modifier: Modifier){
+private fun Header(modifier: Modifier) {
     Column(
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
             .background(color = Color.White),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(modifier = modifier.height(12.dp))
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = modifier.height(24.dp))
+        AppHeader(modifier = modifier)
+        Spacer(modifier = modifier.height(18.dp))
         Text(
             text = stringResource(R.string.welcome_text),
             style = MaterialTheme.typography.headlineSmall,
-
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.SemiBold
         )
-        Spacer(modifier = modifier.height(12.dp))
+        Spacer(modifier = modifier.height(8.dp))
         Text(
             text = stringResource(R.string.onboard_hint),
-            style = MaterialTheme.typography.labelSmall,
-            color = Color.DarkGray
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(start = 12.dp, end = 12.dp),
+            style = MaterialTheme.typography.labelMedium,
+            color = Color.DarkGray,
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
         )
+        Spacer(modifier = modifier.height(20.dp))
     }
 }
+private val RoundedCornerRadius = 12.dp
 
 @Preview
 @Composable
