9c06e9ff2d5293d7b82102e74628dbe4a376348c
jenAndroidDev <jendev971@gmail.com>
2024-12-04 22:32:27 +0530

    Fix :ForgotPasswordRoute 🔨

--- app/src/main/java/com/example/composesignup/feature/onboard/navigation/ForgotPasswordRoute.kt
+++ app/src/main/java/com/example/composesignup/feature/onboard/navigation/ForgotPasswordRoute.kt
@@ -1,12 +1,9 @@
 package com.example.composesignup.feature.onboard.navigation
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.composesignup.feature.onboard.presentation.ForgotPasswordScreen
 
 
 @Composable
@@ -14,11 +11,5 @@ fun ForgotPasswordRoute(
     modifier: Modifier= Modifier,
     onClick:()->Unit
 ){
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "ForgotPassword Screen Coming Soon")
-    }
-
+    ForgotPasswordScreen(modifier = modifier)
 }
