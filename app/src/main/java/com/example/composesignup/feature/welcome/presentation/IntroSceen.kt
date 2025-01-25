6a636d3edc7d95db422fc70b88d94badcac2578f
jenAndroidDev <jendev971@gmail.com>
2025-01-25 11:27:58 +0530

    1.🔨Edge To Edge For Intro Screen.

--- app/src/main/java/com/example/composesignup/feature/welcome/presentation/IntroSceen.kt
+++ app/src/main/java/com/example/composesignup/feature/welcome/presentation/IntroSceen.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
@@ -74,6 +78,9 @@ fun IntroScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
+            .windowInsetsPadding(
+                WindowInsets.statusBars
+            )
             .background(
                  color = MaterialTheme.colorScheme.background),
                  horizontalAlignment = Alignment.CenterHorizontally,
